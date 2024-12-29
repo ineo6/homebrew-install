1f3bfa0452350360dbabb503532514973da9c7fc
neo <arklove@qq.com>
2024-12-29 16:50:04 +0800

    feat: update

--- .umirc.ts
+++ .umirc.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   mode: 'site',
   logo: '/images/homebrew-256x256.png',
   define: {
-    'AVALON_SERVER': process.env.NODE_ENV === 'production' ? 'https://avalon.idayer.com' : 'http://localhost:8000',
+    'AVALON_SERVER': process.env.NODE_ENV === 'production' ? 'https://api.idayer.com' : 'http://localhost:8000',
   },
   favicon: '/favicon.ico',
   locales: [['zh-CN', '中文']],
